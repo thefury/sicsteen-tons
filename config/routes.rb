35120c7f3da6a2c42586f6fbcd51d900549d0f8c
Trevor Oke <toke@infotech.com>
2014-12-18 13:30:23 -0500

    add api controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
 
   root 'site#client'
 
+  namespace :api do
+    namespace :v1 do
+      resources :requests, only: [:index]
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
