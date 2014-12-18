2967ce291806379324aa96a0a5c911661383e851
Ravi Desai <rdesai@infotech.com>
2014-12-18 16:38:04 -0500

    cull requests controller

--- spec/controllers/api/v1/requests_controller_spec.rb
+++ spec/controllers/api/v1/requests_controller_spec.rb
@@ -1,57 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::RequestsController, :type => :controller do
-  
-  describe "index" do
-    
-    it "should render an array of requests" do
-      request_1 = Request.create floor: "1"
+  describe "create" do
+    context "successfully creates" do
+      it "one request entry" do
+        post :create, floor: "1"
 
-      get :index
-      result = JSON.parse response.body
-      
-      expect(result[0]['id']).to be(request_1.id)
+        expect(Request.count).to eq(1)
+      end
+
+      it "and returns status 201" do
+        post :create, floor: "1"
+
+        expect(response.status).to eq(201)
+      end
     end
 
-    it "should render only active requests" do
-      request_1 = Request.create floor: "1"
-      request_2 = Request.create floor: "2", deleted: true
+    context "on failed creation" do
+      it "doesn't change the count of objects" do
+        post :create
 
-      get :index
-      result = JSON.parse response.body
-      
-      expect(result.map{|i| i['id'] }).to_not include(request_2.id)
+        expect(Request.count).to eq(0)
+      end
+
+      it "returns a status 422" do
+        post :create, floor: "22"
+
+        expect(response.status).to eq(422)
+      end
     end
-
   end
-
-  describe "destroy" do
-    
-    it "should destroy a request" do
-      request = Request.create floor: "1"
-
-      delete :destroy, id: request.id
-      
-      expect(Request.find(request.id).deleted).to be true
-    end
-
-    it "should return 200 on successful deletion" do
-      request = Request.create floor: "1"
-
-      delete :destroy, id: request.id
-      
-      expect(response).to have_http_status 200
-    end
-
-    it "should return 422 on unsuccessful deletion" do
-      request = Request.create floor: "1"
-      Request.any_instance.stub(:update_attributes).and_return false
-
-      delete :destroy, id: request.id
-      
-      expect(response).to have_http_status 422
-    end
-
-  end
-
 end
